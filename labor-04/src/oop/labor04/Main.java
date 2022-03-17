934da136f1fc84b2ece986f9cb5725c0521a08d6
Laczkó-Angi Tamás <99878612+Laczko-AngiTamas@users.noreply.github.com>
2022-03-17 16:11:56 +0200

    Update Main.java

--- labor-04/src/oop/labor04/Main.java
+++ labor-04/src/oop/labor04/Main.java
@@ -15,7 +15,7 @@ public class Main {
             System.out.println(p);
         }
     }
-// ....
+
     public static void readFilePrintItsLineNumbered(String fileName) {
 
         try (Scanner scanner = new Scanner(new File(fileName))) {
